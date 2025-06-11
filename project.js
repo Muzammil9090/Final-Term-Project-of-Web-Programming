ed03fa2c7800bec8767fa09280749689a72f34d5
Muzammil9090 <70145807@student.uol.edu.pk>
2025-06-11 16:41:05 +0500

    Add files via upload

---
+++ project.js
@@ -0,0 +1,14 @@
+
+const parts = ["Engine", "Brakes", "Transmission", "Interior"];
+parts.map(part => console.log(`Available part: ${part}`));
+
+function showMessage(msg) {
+  alert(`Message: ${msg}`);
+}
+
+try {
+  let tyreCount = 4;
+  if (tyreCount !== 4) throw "Tyres must be 4!";
+} catch (err) {
+  console.error("Error:", err);
+}
